44725938a50bd4a088ea6d6a79ea2d065137b86f
nojohn04 <nojohn04@wsc.edu>
2026-02-11 10:19:44 -0600

    added throwing an error

--- src/Account.java
+++ src/Account.java
@@ -29,5 +29,18 @@ public class Account {
     public static int getLastId() {
         return lastId;
     }
+
+
+    /**
+     * Allow setting of the account owner's last name.
+     * @param lastName the account owner's last name.
+     * @throws IllegalArgumentException Invalid last name! Last name can't be null or empty for id: 1
+     */
+    public void setLastName(String lastName) {
+        if (lastName == null || lastName.isEmpty())
+            throw new IllegalArgumentException("Invalid last name! Name can't be blank for id: " + id);
+        else
+            this.lastName = lastName;
+    }
 }
 
